7ba4a00dfa8ee68b519a00c9493735635b160f24
febingeorge10 <febingeorge0@gmail.com>
2021-09-21 17:10:44 +0530

    initial committ

---
+++ src/main/java/com/springboott/AwsSpringBootApplic/AwsSpringBootApplicApplication.java
@@ -0,0 +1,13 @@
+package com.springboott.AwsSpringBootApplic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringBootApplicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringBootApplicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/springboott/AwsSpringBootApplic/HelloController.java
@@ -0,0 +1,13 @@
+package com.springboott.AwsSpringBootApplic;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hai helllo welcome";
+	}
+}
